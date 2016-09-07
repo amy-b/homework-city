b55ff5d1247352dc71194db8c9a7e80ad18c2c45
amy-b <amy.berkhoudt@gmail.com>
2016-09-06 21:37:12 -0700

    Completes Code

--- Assignment/starter_code/js/index.js
+++ Assignment/starter_code/js/index.js
@@ -1,41 +1,33 @@
 $(document).ready(function(){
-console.log("ready");
+//console.log("ready");
 
 //When user types city name and clicks submit, a photo of the city will appear in the background.
-	$("submit-btn").submit(changePhoto)
-	var city = ("nyc", "la", "sf", "atx", "syd");
+	$("submit-btn").click(changeBackground);
 
-//If user submits:
-//"New York" or "New York City" or "NYC" make the background of the page nyc.jpg
-	if (city === "New York" || city === "New York City" || city === "NYC"){
-		console.log("new york")
-		$("body").attr("class", "nyc");
-		$(this).prev("input").val();
-//if not, keep default 
-	}
-//"San Francisco" or "SF" or "Bay Area" make the background of the page sf.jpg
-	else if (city === "San Francisco" || city === "SF" || city === "Bay Area"){
-		console.log("run");
+	function changeBackground(){
+		event.preventDefault();
+		var city = $('#city-type').val(); //grabs user input and stores as a variable
+		
+		//"New York" or "New York City" or "NYC" make the background of the page nyc.jpg
+		if (city === "New York" || city === "New York City" || city === "NYC"){
+			$("body").attr("class", "nyc");
+		
+		//"San Francisco" or "SF" or "Bay Area" make the background of the page sf.jpg
+		} else if (city === "San Francisco" || city === "SF" || city === "Bay Area"){
 		$("body").attr("class", "sf");
-		$(this).prev("input").val();
-	}
-//"Los Angeles" or "LA" or "LAX" make the background of the page la.jpg
-	if (city === "Los Angeles" || city === "LA" || city === "LAX"){
-		console.log("run");
+		
+		//"Los Angeles" or "LA" or "LAX" make the background of the page la.jpg
+		} else if (city === "Los Angeles" || city === "LA" || city === "LAX"){
 		$("body").attr("class", "la");
-		$(this).prev("input").val();
+
+		//"Austin" or "ATX" make the background of the page austin.jpg
+		} else if (city === "Austin" || city === "ATX"){
+		$("body").attr("class", "atx");
+
+		//"Sydney" or "SYD" make the background of the page sydney.jpg
+		} else if (city === "Sydney" || city === "SYD"){
+		$("body").attr("class", "syd");
+		}
 	}
-//"Austin" or "ATX" make the background of the page austin.jpg
-
-//"Sydney" or "SYD" make the background of the page sydney.jpg
-
-	});
-function changePhoto(){
-	event.preventDefault();
-	var city = $("#city-type").val();
-	console.log(city)
-}
-
-
-
+});
 
